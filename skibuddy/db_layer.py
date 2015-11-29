aa34f47a956ef89b9c214df7bc7a7e3c02ec66d3
Purvi Singla <purvip@dessci.com>
2015-11-28 17:03:46 -0800

    create_event

--- skibuddy/db_layer.py
+++ skibuddy/db_layer.py
@@ -8,6 +8,7 @@ class db_layer:
     def get_data(self,cond):
         print cond
         cur = self.coll.find(cond)
+        #print "hi9", cur
         l = []
         for i in cur:
             del i['_id']
@@ -15,8 +16,19 @@ class db_layer:
             l.append(i)
         return l
 
+    def get_count(self):
+        cur = self.coll.find({},{"count":1})
+        res= cur[0]['count']
+        return res
+
+    def set_count(self,record):
+        print "Inside set_count "
+        print record
+        cur = self.coll.save({"_id":"event_id_check","count":record})
+
 #expect records as list of records
     def set_data(self,records):
         self.coll.insert_many(records)
+
 
 
--- skibuddy/views.py
+++ skibuddy/views.py
@@ -4,6 +4,20 @@ import db_layer
 from django.views.decorators.csrf import csrf_exempt
 import json
 
+
+def autoIncrement():
+    cnt=0;
+    print "A"
+    db = db_layer.db_layer('count')
+    print "B"
+    l = db.get_count()
+    print "C"
+    cnt=l+1;
+    db.set_count(cnt)
+    print "D"
+    return cnt
+
+
 # Create your views here.
 
 # always return JSON objects in the format
@@ -21,7 +35,6 @@ def check_user(request):
     print data
     db = db_layer.db_layer('user')
     l = db.get_data({'user_id':data['data'][0]['user_id']})
-    print "hi"
     print l
     if not l:
         db.set_data(data['data'])
@@ -36,15 +49,17 @@ def check_user(request):
 @csrf_exempt
 def create_event(request):
     data = ((request.body))
-    print data
     data = data.replace("'", "\"")
     data = json.loads(data)
-    print data
+    data['data'][0]['event_id']=autoIncrement()
     if data == '':
         return HttpResponse(json.dumps({'data':"no data received",'status':"failed"}), content_type="application/json")
     db = db_layer.db_layer('ski_event')
     db.set_data(data['data'])
     return HttpResponse(json.dumps({'data':"Event created Successfully",'status':"success"}), content_type="application/json")
+
+#@csrf_exempt
+#def join_event(request):
 
 
 #to get all  pass {} to get selected pass query
